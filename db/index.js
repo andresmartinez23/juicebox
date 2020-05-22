454def67b7654e2b9df1134fd0857b5003c28bd3
Andres Soto-Martinez <andressoto-martinez@Andres-Soto-Martinezs-MacBook-Pro.local>
2020-05-21 20:15:20 -0400

    FINISHED DAY 2 PART 2

--- db/index.js
+++ db/index.js
@@ -12,9 +12,9 @@ async function createUser({ username, password, name, location }) {
       rows: [user],
     } = await client.query(
       `
-      INSERT INTO users(username, password, name, location) 
-      VALUES($1, $2, $3, $4) 
-      ON CONFLICT (username) DO NOTHING 
+      INSERT INTO users(username, password, name, location)
+      VALUES($1, $2, $3, $4)
+      ON CONFLICT (username) DO NOTHING
       RETURNING *;
     `,
       [username, password, name, location]
@@ -59,7 +59,7 @@ async function updateUser(id, fields = {}) {
 async function getAllUsers() {
   try {
     const { rows } = await client.query(`
-      SELECT id, username, name, location, active 
+      SELECT id, username, name, location, active
       FROM users;
     `);
 
@@ -113,7 +113,7 @@ async function createPost({ authorId, title, content, tags = [] }) {
       rows: [post],
     } = await client.query(
       `
-      INSERT INTO posts("authorId", title, content) 
+      INSERT INTO posts("authorId", title, content)
       VALUES($1, $2, $3)
       RETURNING *;
     `,
@@ -233,7 +233,7 @@ async function createTags(tagList) {
   try {
     await client.query(
       `
-        INSERT INTO tags(name) 
+        INSERT INTO tags(name)
       VALUES(${insertValues})
       ON CONFLICT (name) DO NOTHING;
         `,
@@ -350,6 +350,25 @@ async function getPostsByTagName(tagName) {
   }
 }
 
+async function getUserByUsername(username) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+      SELECT *
+      FROM users
+      WHERE username=$1
+    `,
+      [username]
+    );
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
 module.exports = {
   client,
   createUser,
@@ -366,85 +385,5 @@ module.exports = {
   createPostTag,
   getPostsByTagName,
   getAllTags,
+  getUserByUsername,
 };
-
-// const { Client } = require("pg");
-
-// const client = new Client("postgres://localhost:5432/juicebox-dev");
-
-// module.exports = { client };
-
-// async function getAllUsers() {
-//   const { rows } = await client.query(
-//     `SELECT id, username
-//         FROM users;
-//         `
-//   );
-
-//   return rows;
-// }
-
-// // async function createUser({ username, password, name, location }) {
-// //   const { rows } = await client.query(
-// //     `
-// //          INSERT INTO users(username, password)
-// //          VALUES($1, $2)
-// //          ON CONFLICT (username) DO NOTHING
-// //          RETURNING *;
-// //        `,
-// //     [username, password, name, location]
-// //   );
-
-// //   return rows;
-// // }
-
-// async function createUser({ username, password, name, location }) {
-//   try {
-//     const { rows } = await client.query(
-//       `
-//         INSERT INTO users(username, password)
-//         VALUES($1, $2)
-//         ON CONFLICT (username) DO NOTHING
-//         RETURNING *;
-//       `,
-//       [username, password, name, location]
-//     );
-
-//     return rows;
-//   } catch (error) {
-//     throw error;
-//   }
-// }
-
-// async function updateUser(id, fields = {}) {
-//   const setString = Object.keys(fields)
-//     .map((key, index) => `"${key}"=$${index + 1}`)
-//     .join(", ");
-
-//   if (setString.length === 0) {
-//     return;
-//   }
-
-//   try {
-//     const result = await client.query(
-//       `
-//         UPDATE users
-//         SET ${setString}
-//         WHERE id=${id}
-//         RETURNING *;
-//         `,
-//       Object.values(fields)
-//     );
-
-//     return result;
-//   } catch (error) {
-//     throw error;
-//   }
-// }
-
-// module.exports = {
-//   client,
-//   getAllUsers,
-//   createUser,
-//   updateUser,
-// };
--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const PORT = 3000;
+require("dotenv").config();
 const express = require("express");
 const server = express();
 const apiRouter = require("./api");
+const bodyParser = require("body-parser");
+
+const morgan = require("morgan");
 const { client } = require("./db");
+
 client.connect();
+
+server.use(bodyParser.json());
 
 server.listen(PORT, () => {
   console.log("The server is up on port", PORT);
 });
+
+server.use(morgan("dev"));
 
 server.use("/api", apiRouter);
 
 server.use((req, res, next) => {
   next();
 });
-
-const bodyParser = require("body-parser");
-server.use(bodyParser.json());
-
-const morgan = require("morgan");
-server.use(morgan("dev"));
